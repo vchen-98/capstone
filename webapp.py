65dfd1541ba125543ae1f48d04fd4531f3f93459
Victor Chen <victor.i.chen.98@gmail.com>
2021-09-21 12:48:35 -0700

    vc daily progress - streamlit webapp

--- webapp.py
+++ webapp.py
@@ -1,3 +1,5 @@
+# https://www.youtube.com/watch?v=bEOiYF1a6Ak
+
 import streamlit as st 
 import joblib,os
 import spacy
@@ -8,6 +10,16 @@ import matplotlib
 matplotlib.use("Agg")
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+
+# Vectorizer
+review_vectorizer = open('pickle/tfidf_vectorizer', 'rb')
+review_cv = joblib.load(review_vectorizer)
+
+# Load Models
+def load_prediction_models(model_file):
+    loaded_models = joblib.load(open(os.path.join(model_file), 'rb'))
+    return loaded_models
 
 def main():
     '''Review Classifier App with Streamlit'''
@@ -27,7 +39,13 @@ def main():
         prediction_labels = {'Negative': 0, 'Positive': 1}
         if st.button('Classify'):
             st.text('Original Test ::\n{}'.format(review_text))
+            vect_text = review_cv.transform([review_text]).toarray()
+            if model_choice == 'LR': 
+                predictor = load_prediction_models('pickle/lr_classifier_tuned')
+                prediction = predictor.predict()
+                st.write(prediction)
         
+        # 19:05 video timestamp
         
         
     if choice == 'NLP':
