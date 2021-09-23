9a69a4f328aed32544a5a9f28da3a0ab740a545a
Victor Chen <victor.i.chen.98@gmail.com>
2021-09-22 18:19:31 -0700

    vc daily work streamlit webapp

--- webapp.py
+++ webapp.py
@@ -1,5 +1,3 @@
-# https://www.youtube.com/watch?v=bEOiYF1a6Ak
-
 import streamlit as st 
 import joblib,os
 import spacy
@@ -34,15 +32,15 @@ def main():
         st.info('Prediction with ML')
         
         review_text = st.text_area('Enter Text', 'Type Here')
-        all_ml_models = ['Logistic Regression']
+        all_ml_models = ['Logistic Regression', 'NB']
         model_choice = st.selectbox('Choose ML Model', all_ml_models)
         prediction_labels = {'Negative': 0, 'Positive': 1}
         if st.button('Classify'):
             st.text('Original Test ::\n{}'.format(review_text))
             vect_text = review_cv.transform([review_text]).toarray()
             if model_choice == 'LR': 
-                predictor = load_prediction_models('pickle/lr_classifier_tuned')
-                prediction = predictor.predict()
+                predictor = load_prediction_models('pickle/lr_classifier_tuned.pkl')
+                prediction = predictor.predict(vect_text)
                 st.write(prediction)
         
         # 19:05 video timestamp
